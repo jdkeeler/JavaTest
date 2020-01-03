0c4d888fdf39eac2cc15f2f95f14bc95a5b2fb70
Joseph Keeler <joseph.keeler@nike.com>
2020-01-02 16:42:43 -0800

    Trimming down to select questions

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -10,23 +10,6 @@ public class Questions {
     }
 
     //Instructions:
-    //  Given an array of integers, return indices of two numbers that add up to the provided target.
-    //  Return only ONE solution.  You may not use the same index twice.
-    public void Question_TwoSum() {
-
-        //Do NOT modify values below
-        final int[] INPUT_ARRAY = {1, 5, 2, 0, 4, 11, 9, 6, 12, 7};
-        final int TARGET_VAL = 9;
-
-        //YOUR CODE HERE
-
-
-        //OUTPUT TWO INDICES
-        System.out.println("\n\n-----QUESTION_TWOSUM-----\n\n");
-
-    }
-
-    //Instructions:
     //  Given an array of integers, output all numbers appearing more than once.  Only output duplicates
     //  one time.
     public void Question_FindDuplicates() {
@@ -43,47 +26,19 @@ public class Questions {
     }
 
     //Instructions:
-    //  Given an array of digits representing a number, add one to the number and output it
-    //  Example:  In array [1, 2, 4, 5] this translates into number 1245.  You would add one and output the value.
-    public void Question_PlusOne() {
+    //  Given an array of integers, return indices of two numbers that add up to the provided target.
+    //  Return only ONE solution.  You may not use the same index twice.
+    public void Question_TwoSum() {
 
         //Do NOT modify values below
-        final int[] INPUT_ARRAY = {4, 5, 6, 7};
+        final int[] INPUT_ARRAY = {1, 5, 2, 0, 4, 11, 9, 6, 12, 7};
+        final int TARGET_VAL = 9;
 
         //YOUR CODE HERE
 
 
-        //OUTPUT FINAL VALUE
-        System.out.println("\n\n-----QUESTION_PLUSONE-----\n\n");
-
-    }
-
-    //Instructions:
-    //  Given an input string, reverse the value using recursion.
-    public void Question_ReverseString() {
-
-        //Do NOT modify values below
-        final String INPUT_STRING = "This is a string to reverse";
-
-        //OUTPUT REVERSE STRING
-        System.out.println("\n\n-----QUESTION_REVERSESTRING-----\n\n");
-
-    }
-
-    //Instructions:
-    //  Given an input string, see if it's a palindrome.  Filter out any non letters.  Ignore capitalization.
-    //  Example: Racecar is a palindrome.
-    public void Question_Palindrome() {
-
-        //Do NOT modify values below
-        final String test1 = "Racecar1";
-        final String test2 = "ABC123cba";
-        final String test3 = "31Civ23ic";
-
-        //YOUR CODE HERE
-
-        //OUTPUT BOOLEAN VALUES FOR THREE TESTS
-        System.out.println("\n\n-----QUESTION_PALINDROME-----\n\n");
+        //OUTPUT TWO INDICES
+        System.out.println("\n\n-----QUESTION_TWOSUM-----\n\n");
 
     }
 
@@ -102,19 +57,5 @@ public class Questions {
 
     }
 
-    //Instructions:
-    //  Given an input array, output all missing numbers in order.
-    //  Example: In array [7, 4, 3, 2, 1, 10], you would output: 5, 6, 8, 9
-    public void Question_MissingNumbers() {
-
-        //Do NOT modify the values below
-        final int[] INPUT_ARRAY = {4, 5, 1, 2, 10, 8, 7, 14, 20};
-
-        //YOUR CODE HERE
-
-        //OUTPUT MISSING NUMBERS
-        System.out.println("\n\n-----QUESTION_MISSINGNUMBERS-----\n\n");
-
-    }
 
 }
